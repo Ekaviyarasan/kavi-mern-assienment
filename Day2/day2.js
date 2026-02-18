af2b87f740eebf94178dba46c16060d1c54da87d
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-02-18 19:50:25 +0530

    Day2
    js

---
+++ Day2/day2.js
@@ -0,0 +1,68 @@
+var name = "kavi";
+let age = 23;
+const isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+
+let x;
+console.log(x);   // undefined
+
+let y = null;
+console.log(y);   // null
+
+
+let str = "Hello";
+let num = 100;
+let bool = true;
+let big = 12345678901234567890n;
+let empty = null;
+let notAssigned;
+
+
+
+let a = "10";
+let b = "hello";
+
+console.log(a * 2);  // 20
+console.log(b * 2);  // NaN
+
+
+// student name"
+
+// camelCase → studentName
+
+// PascalCase → StudentName
+
+// snake_case → student_name
+
+// kebab-case → student-name
+
+
+let num1 = 20;
+let num2 = 5;
+
+console.log(num1 + num2); // 25
+console.log(num1 - num2); // 15
+console.log(num1 * num2); // 100
+console.log(num1 / num2); // 4
+console.log(num1 % num2); // 0
+
+
+console.log(10 == "10");   // true
+console.log(10 === "10");  // false
+console.log(10 != "10");   // false
+console.log(10 !== "10");  // true
+
+
+
+let age = 20;
+let hasID = true;
+
+if (age > 18 && hasID) {
+  console.log("Eligible");
+} else if (age < 18 || !hasID) {
+  console.log("Not eligible");
+}
