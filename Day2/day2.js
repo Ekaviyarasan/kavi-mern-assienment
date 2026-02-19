26d8fa0db1c8175af661318dd985d724a19c4373
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-02-19 19:11:22 +0530

    update.day2

--- Day2/day2.js
+++ Day2/day2.js
@@ -2,32 +2,32 @@ var name = "kavi";
 let age = 23;
 const isStudent = true;
 
-console.log(name);
-console.log(age);
-console.log(isStudent);
+ console.log(name);
+ console.log(age);
+ console.log(isStudent);
 
 
-let x;
-console.log(x);   // undefined
+// let x;
+// console.log(x);   // undefined
 
-let y = null;
-console.log(y);   // null
-
-
-let str = "Hello";
-let num = 100;
-let bool = true;
-let big = 12345678901234567890n;
-let empty = null;
-let notAssigned;
+// let y = null;
+// console.log(y);   // null
 
 
+// let str = "Hello";
+// let num = 100;
+// let bool = true;
+// let big = 12345678901234567890n;
+// let empty = null;
+// let notAssigned;
 
-let a = "10";
-let b = "hello";
 
-console.log(a * 2);  // 20
-console.log(b * 2);  // NaN
+
+// let a = "10";
+// let b = "hello";
+
+// console.log(a * 2);  // 20
+// console.log(b * 2);  // NaN
 
 
 // student name"
@@ -41,20 +41,20 @@ console.log(b * 2);  // NaN
 // kebab-case → student-name
 
 
-let num1 = 20;
-let num2 = 5;
+// let num1 = 20;
+// let num2 = 5;
 
-console.log(num1 + num2); // 25
-console.log(num1 - num2); // 15
-console.log(num1 * num2); // 100
-console.log(num1 / num2); // 4
-console.log(num1 % num2); // 0
+// console.log(num1 + num2); // 25
+// console.log(num1 - num2); // 15
+// console.log(num1 * num2); // 100
+// console.log(num1 / num2); // 4
+// console.log(num1 % num2); // 0
 
 
-console.log(10 == "10");   // true
-console.log(10 === "10");  // false
-console.log(10 != "10");   // false
-console.log(10 !== "10");  // true
+// console.log(10 == "10");   // true
+// console.log(10 === "10");  // false
+// console.log(10 != "10");   // false
+// console.log(10 !== "10");  // true
 
 
 
