cc512d99bf35df4a8077aaf269ed86f12c2bf068
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-02-25 13:29:42 +0530

    practice

---
+++ js practice/all.js
@@ -0,0 +1,58 @@
+// // // for(let i=0; i>=10; i++){
+// // //     console.log(i)
+// // // }
+// // // for (let i = 10; i >= 0; i--) {
+// // //   console.log(i);
+// // // }
+
+// // const obj = {
+// //   name: "Alex",
+// //   age: 25,
+// //   city: "NY"
+// // };
+
+// // for (const key in obj) {
+// //   console.log(key, obj[key]);
+// // }
+
+
+// // const users = [
+// //   { name: "silam", age: 22 },
+// //   { name: "aura", age: 30 },
+// //   { name: "arasan", age: 27 }
+// // ];
+
+// // for (let i = 0; i < users.length; i++) {
+// //   console.log(users[i].name, users[i].age);
+// // }
+
+// // const kavi=[1,2,3,4];
+// // for( let i=0; i<kavi.length; i++){
+// //     console.log(kavi[i/2])
+// // }
+
+// // for(let i=20; i>=0; i--){
+// //     console.log(i)
+// // }
+
+// // const add={
+// //     name:"hr",
+// //     age:10
+// // }
+// // for(const key in add){
+// //     console.log(key,add[key])
+// // }
+
+
+
+
+// setInterval(()=>{
+//      const add={
+//      name:"hr",
+//      age:10
+//  }
+//  for(const key in add){
+//      console.log(key,add[key])
+// }
+
+// },2000)
