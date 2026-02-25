2a460b660e6400e87734e3603dcebb869568d7d7
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-02-25 11:22:30 +0530

    Day5 Update

--- Day5/day5.js
+++ Day5/day5.js
@@ -1,117 +1,40 @@
- const key=(a,b)=>{
-     return  a/b
+// let original = [[1, 2], [3, 4]];
 
-    
- }
+// let shallow = original.slice();
 
- console.log(key(73766,5))
+// shallow[0][0] = 99;
 
- const kavi =(x,y)=>x+y
- console.log(kavi(20,70))
-
- const days =(a,b,roll)=>{
-     return  roll(a,b)
-    
- }
- const value=(x,y)=>x+y
- const roll=(x,y)=>x*y
- console.log(days (20,70,(x,y)=>{x+y}))
- console.log(days(20,70,roll))
-
-
-const aji=(cb,name)=>{
-    return cb(name)
-    
-}
-const h=(m)=>m
-const r=(n)=>n
-console.log(aji(h,"java"))
-console.log(aji(r,"react"))
-
-
- 
+// console.log(original); 
+// console.log(shallow);  
 
 
 
+let original = [[10, 20], [30, 40]];
+
+let shallow = [...original];
+
+shallow[1][1] = 888;
+
+console.log(original); // [[10, 20], [30, 888]]
+console.log(shallow);  // [[10, 20], [30, 888]]
 
 
- 
+Deepcopy
+// let original = [[1, 2], [3, 4]];
 
- const processArray = (arr,callback)=>{
-    
-     const result = []
-   
-     for(let i = 0; i < arr.length; i++){
+// let deep = original.map(arr => [...arr]);
 
-         result.push(callback(arr[i]))
+// deep[0][0] = 77;
 
-     }
- 
-
-     return result
-    
- }
-
- const squre = (num)=>num*num 
+// console.log(original); 
+// console.log(deep);     
 
 
- console.log(processArray([1,2,3,4],squre));
+// let original = [[5, 6], [7, 8]];
 
+// let deep = JSON.parse(JSON.stringify(original));
 
+// deep[1][0] = 999;
 
-
-
-
- const validation = (value,rule) => rule(value);
-
-
- const isPositive = (num)=> num > 0 
-
- const isEven = (num)=> num % 2===0 
-
-
-
- console.log(validation(10,isPositive));
- console.log(validation(10,isEven));
-
-
-
-
- setTimeout(()=>{
-     console.log(validation(10,isPositive));
- },2000)
-
-
- const inter=()=>{
-    console.log("hi")
-}
-setInterval(inter,1000)
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
+// console.log(original); 
+// console.log(deep);     
