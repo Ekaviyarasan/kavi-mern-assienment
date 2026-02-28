5ed9e8dcb754d653cb807500397e79372e50c9f3
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-02-28 14:42:09 +0530

    update Day7

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,99 +1,43 @@
-
-
-
-    //  let a = 10;
-    //  console.log(`This is the value of the ${a}
-    //  this is the process
-    
-    //  `);
-    
- Destructuring
-
- array
- const arr = [1,2,3,4,5,6]
-
- const [a,b,c] = arr
-
- console.log(a,b,c);
-
-
-//  object Destructuring
-
-// const obj = {
-//     name:"React",
-//     subject:"Node"
+// function createProfile(user) {
+//   const { name, age, role = "User" } = user;
+//   return `${name} is ${age} years old and works as ${role}`;
 // }
 
-// const {name,subject} = obj
+
+// createProfile({ name: "kavi", age: 22 });
+
+const calculateTotal = (discount, ...prices) => {
+  const total = prices.reduce((sum, price) => sum + price, 0);
+  return total - (total * discount) / 100;
+};
 
 
-// console.log(subject);
-
-
- const arr1 = [1,2,3,4,5]
-
- console.log('original array',arr1);
-
-// const copyarry = [...arr1]
-
-
-  
- const adding = [...arr1,45,658,87]
-
- console.log(adding);
-
- const merge = [...arr1]
-
-
- console.log(merge);
+calculateTotal(10, 100, 200, 300);
 
 
 
-
-// Object Speart
-
- const obj = {
-     name:"Dhoni",
-     year:"2011"
- }
-
- console.log(obj);
-
-
- const data = {...obj,year:2026,city:"Chennai"}
-
- console.log(data);
+function createUser(key, value) {
+  return {
+    [key]: value,
+    display() {
+      console.log(`${key}: ${value}`);
+    }
+  };
+}
 
 
-// rest opretrs
-
-// const add = (...a)=>{
-
-   
-
-//     const data = a.filter((e)=>e===1)
-    
-//  console.log(data);
-// }
-
-// console.log(add(1,2,1,4,5,1,3,4));
+createUser("role", "Admin");
 
 
 
-// let data="this is data";
+const users = [
+  { name: "A", marks: 80 },
+  { name: "B" },
+  { name: "C", marks: 60 }
+];
 
-// console.log(data??10);
+const result = users.map(
+  user => `${user.name} scored ${user.marks ?? 0}`
+);
 
-
-
-// console.log(data);
-
- const kl = {data:"player"}
-
- const jaddu = {kl}
-
- console.log(jaddu);
-
-
-
-
+result;
