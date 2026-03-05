2a53cbb12cb2170327a455e762e45eee93655580
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-03-05 20:36:50 +0530

    Day12

---
+++ REACT/Day12/ass/vite/class/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hello from './Hollo'
+import MyComponent from './Mycompont'
+import Counter from './Counter'
+
+function App() {
+  return (
+    <>
+    <Hello/>
+    <MyComponent/>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ REACT/Day12/ass/vite/class/src/Hollo.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react"
+
+
+
+class Hello extends Component{
+    render(){
+        return(<>
+        <button>click</button>
+
+        </>)
+    }
+}
+export default Hello
