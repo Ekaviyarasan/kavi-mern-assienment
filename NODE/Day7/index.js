24a78968ed5cca6a79c80ade3eba96177a02c011
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-04-16 13:57:31 +0530

    Day7

---
+++ NODE/Day7/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db"
+
+
+dotenv.config()
+
+const app=express()
+
+connectDB()
+
+const PORT=process.env.PORT||5000
+
+app.listen(PORT,()=>{
+    console.log(`server run...http://localhost:${PORT}`);
+    
+})
