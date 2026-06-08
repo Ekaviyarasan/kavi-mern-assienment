49f73e9e4ee6f57cd09286c4b81b201375de8a5c
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-06-08 14:01:33 +0530

    day1

---
+++ js practice/Day1/day1.js
@@ -0,0 +1,56 @@
+// function add (a,b,c){
+//     console.log(a+b%c);
+    
+// }
+// add(49,70,5)
+
+// function rolex (name){
+//     console.log("hi" +name);
+    
+// }
+// rolex("surya")
+
+
+// function process(a, b, callback) {
+//     return callback(a, b);
+// }
+
+// let result = process(10, 5, (x, y) => {
+//     return x + y;
+// });
+
+// console.log(result); 
+
+
+
+// function outerFunction() {
+//     const name = "ajith";
+
+//     function innerFunction() {
+//         console.log("Hello " + name);
+//     }
+
+//     return innerFunction;
+// }
+
+// const result = outerFunction();
+// result();
+
+// let student = {
+//     name: "Ismail",
+//     age: 25,
+//     city: "Chennai"
+// };
+
+// for (let key in student) {
+//     console.log(key + " : " + student[key]);
+// }
+
+
+// let numbers = [1, 2, 3, 4];
+
+// let doubled = numbers.map((value) => {
+//     return value * 2;
+// });
+
+// console.log(doubled);
